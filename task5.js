049e1e47b2f8a6626b5260882ac9670bb9f45f95
Ahmad Faisal <achmad_fsl@yahoo.com>
2024-02-25 11:46:58 +0700

    add task 5

---
+++ task5.js
@@ -0,0 +1,14 @@
+/**
+  Buat function-function berikit dari sample data
+  1. untuk menambahkan array object
+  2. untuk menghapus object dalam array
+  3. untuk mengedit value name dalam array objecy
+  4. untuk memanipulasi data array object dari id
+ */
+
+// Sample Data
+let people = [
+  { id: 1, name: 'John', age: 25 },
+  { id: 2, name: 'Jane', age: 30 },
+  { id: 3, name: 'Bob', age: 22 }
+];
